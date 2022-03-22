0026506644a1cc404a18ebb9a2dbfa4563a882bf
crus <crus@frodo.localdomain>
2022-03-21 20:57:04 -0500

    Add SwiftVocabulary Project with UI

---
+++ SwiftVocabulary/SwiftVocabulary/DefinitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by crus on 3/21/22.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    @IBOutlet weak var vocabularyLabel: UILabel!
+    
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
